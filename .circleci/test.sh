2de47923a5f10421c728d9986cd97d779d8abe04
lempiji <lempiji@gmail.com>
2019-07-28 17:54:32 +0900

    Update script

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -3,14 +3,7 @@
 set -e
 set -u
 
-curl https://dlang.org/install.sh | bash -s
-
-~/dlang/install.sh dmd -a
-ls /home/circleci
-ls /home/circleci/dlang
-ls ~/dlang
-
-source $(~/dlang/install.sh dmd -a)
+source "$(curl -fsS  --retry 3 https://dlang.org/install.sh | bash -s $1 --activate)"
 
 dub test
 dub build --build=docs
