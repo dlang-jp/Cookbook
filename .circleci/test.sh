d4e3ed53566e5e36f1a93d5137817795869f3f50
lempiji <lempiji@gmail.com>
2019-07-28 17:25:40 +0900

    Update scripts

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -3,7 +3,8 @@
 set -e
 set -u
 
-source "$(curl -fsS https://dlang.org/install.sh | bash -s $1 --activate)"
+curl -fsS https://dlang.org/install.sh | bash -s dmd
+source $(~/dlang/install.sh dmd -a)
 
 dub test
 dub build --build=docs
