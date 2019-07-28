9b65ab3406ca81970f05f3d675c343492876b423
lempiji <lempiji@gmail.com>
2019-07-28 17:08:42 +0900

    Update scripts

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 git config user.name CircleCI
 git config user.email <>
 git add -A docs
--- .circleci/test.sh
+++ .circleci/test.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 curl -fsS https://dlang.org/install.sh | bash -s dmd
 source $(~/dlang/install.sh dmd -a)
 dub test
