af9ae3ca985bd32d6bbd5e4f30568a8c9bac0b4d
lempiji <lempiji@gmail.com>
2019-07-28 17:03:46 +0900

    Update config.yml

---
+++ .circleci/deploy.sh
@@ -0,0 +1,4 @@
+git config user.name CircleCI
+git config user.email <>
+git add -A docs
+git commit -m '[skip ci] Update docs'
---
+++ .circleci/test.sh
@@ -0,0 +1,4 @@
+curl -fsS https://dlang.org/install.sh | bash -s dmd
+source $(~/dlang/install.sh dmd -a)
+dub test
+dub build --build=docs
