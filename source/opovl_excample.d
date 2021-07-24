923bd737f7afc522b963327953d688247b5abfee
SHOO <zan77137@nifty.com>
2021-07-24 17:59:43 +0900

    演算子オーバーロードをテンプレート制約や特殊化で行う事例の追加

--- source/opovl_excample.d
+++ source/opovl_excample.d
@@ -34,8 +34,12 @@ opBinary: 二項演算子のオーバーロード
 
 先述のBigIntに代表されるように、組み込み型以外のユーザー定義型であっても、組み込み型と似た特徴を持つ(同じように演算子が使える)ようにすることができるのが、演算子オーバーロードです。
 ここでは代表例として、opBinaryという、二項演算子全般をオーバーロードする方法を使って、四則演算できるようにします。
+opBinaryはテンプレートのメンバー関数として定義します。これを定義すると、足し算や引き算等二項演算子を使用した際、テンプレート引数に使用された二項演算子の演算子が渡されます。
 
 opBinaryは二項演算子であればよいので、+, -, *, / 以外にも、 ~ や & 等の演算子でも利用できます。
+
+See_Also:
+    - https://dlang.org/spec/operatoroverloading.html#binary
 +/
 @safe unittest
 {
@@ -43,12 +47,14 @@ opBinaryは二項演算子であればよいので、+, -, *, / 以外にも、 
     {
         int data;
         // 足し算の演算子オーバーロード
+        // テンプレート制約を使った例
         Integer opBinary(string op)(Integer x) if (op == "+")
         {
             return Integer(data + x.data);
         }
         // 引き算の演算子オーバーロード
-        Integer opBinary(string op)(Integer x) if (op == "-")
+        // テンプレートの特殊化を使った例
+        Integer opBinary(string op: "-")(Integer x)
         {
             return Integer(data - x.data);
         }
@@ -81,6 +87,9 @@ opUnary: 単項演算子のオーバーロード
 
 ` auto b = -a; ` の `-a` (プラスマイナスの符号を逆転する)のように、単項に対する演算子があります。
 たとえば、`-`に加えて'+'、`*`(ポインタ外し), `~`(ビット反転), `!`(否定), `++`(インクリメント), `--`(デクリメント)が相当します。
+
+See_Also:
+    - https://dlang.org/spec/operatoroverloading.html#unary
 +/
 @safe unittest
 {
